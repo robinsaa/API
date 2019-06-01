f9d12c23439cb444d778a7de84ef5f10c2749da8
devarihant <ajai0003@student.monash.edu>
2019-06-02 08:31:44 +1000

    commit database dev credentials

--- db-config.js
+++ db-config.js
@@ -10,7 +10,14 @@ const config = {
 };
 */
 
-
+// Dev credentials
+const config = {
+    host: 'pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'r5eclbki8gq7bgg5',
+    password: 'hjcaku8tztilzcui',
+    database: 'rhs5pq76ew4ry8b8'
+};
 
 // Create a MySQL pool
 const pool = mysql.createPool(config);
